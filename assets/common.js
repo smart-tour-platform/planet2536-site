b461936094acf6b68ecf29677c612ce44a1e5361
akman12914 <rnlduadns98@naver.com>
2026-09-03 15:07:24 +0900

    feat: 서비스명 전체를 플래닛피플(PlanetPeople)로 변경
    드림시큐리티 PG 심사 브랜드명 일치 요건 충족을 위해
    사이트 전반의 '프롬더허들' 표기를 '플래닛피플'로 교체

--- assets/common.js
+++ assets/common.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   const nav = `
   <div class="nav"><div class="wrap">
-    <a class="brand" href="index.html">프롬더허들<span class="dot">.</span></a>
+    <a class="brand" href="index.html">플래닛피플<span class="dot">.</span></a>
     <nav class="nav-links">
       <a href="index.html">소개</a>
       <a href="sessions.html">세션 둘러보기</a>
@@ -10,9 +10,9 @@ document.addEventListener("DOMContentLoaded", () => {
   </div></div>`;
   const footer = `
   <footer><div class="wrap">
-    <div class="f-brand">프롬더허들</div>
+    <div class="f-brand">플래닛피플</div>
     <div class="f-info">
-      상호 : 프롬더허들 &nbsp;|&nbsp; 대표 : 김존<br>
+      상호 : 플래닛피플 &nbsp;|&nbsp; 대표 : 김존<br>
       사업자등록번호 : 413-01-65526 &nbsp;|&nbsp; 통신판매업신고 : 2025-대전유성-0700<br>
       주소 : (34134) 대전 유성구 대학로 99, 충남대학교 산학연교육연구관 별관동 3층 309-1호<br>
       전화 : 010-5062-1625
